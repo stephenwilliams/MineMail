c0571eb7372d6a29f1a854f1dc1f5d6ae330caaa
unknown <stephendwilliams7@gmail.com>
2011-05-18 22:16:12 -0400

    Added loading to the Settings File

--- src/com/alta189/minemail/config/MainSettingsFile.java
+++ src/com/alta189/minemail/config/MainSettingsFile.java
@@ -26,10 +26,10 @@ public class MainSettingsFile {
 	public Integer errorColor = 7;
 	public Boolean iConomyEnabled = false;
 	public Integer costSend = 10;
-	public Integer costRecieve = 5;
+	public Integer costReceive = 5;
 	public Integer costLongSend = 20;
 	public Boolean OPfree = true;
-	
+
 	
 	public MainSettingsFile(MineMail instance) {
 		this.plugin = instance;
@@ -77,6 +77,91 @@ public class MainSettingsFile {
 		
 		this.FileContents = loadFileContents();
 		
+		/*public Integer primaryColor = 10;
+		public Integer secondaryColor = 12;
+		public Integer headerColor = 8;
+		public Integer errorColor = 7;
+		public Boolean iConomyEnabled = false;
+		public Integer costSend = 10;
+		public Integer costRecieve = 5;
+		public Integer costLongSend = 20;
+		public Boolean OPfree = true;*/
+		
+		//Get the Color Values\\
+		if (this.containsKey("headercolor")) {
+			this.headerColor = this.getPropertyInteger("headercolor");
+		}
+		if (this.containsKey("secondarycolor")) {
+			this.secondaryColor = this.getPropertyInteger("headercolor");
+		}
+		if (this.containsKey("primarycolor")) {
+			this.primaryColor = this.getPropertyInteger("primarycolor");
+		}
+		if (this.containsKey("errorcolor")) {
+			this.errorColor = this.getPropertyInteger("errorcolor");
+		}
+		
+		//Check Color values\\
+		if (this.headerColor > 14 || this.headerColor < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, HeaderColor is not valid. MineMail will use default");
+			this.headerColor = 8;
+		}
+		if (this.primaryColor > 14 || this.primaryColor < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, PrimaryColor is not valid. MineMail will use default");
+			this.headerColor = 8;
+		}
+		if (this.secondaryColor > 14 || this.secondaryColor < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, SecondaryColor is not valid. MineMail will use default");
+			this.headerColor = 8;
+		}
+		if (this.errorColor > 14 || this.errorColor < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, ErrorColor is not valid. MineMail will use default");
+			this.headerColor = 8;
+		}
+		
+		//Load iConomy Settings\\
+		if (this.containsKey("turn_iconomy_on")) {
+			this.iConomyEnabled = this.getPropertyBoolean("turn_iconomy_on");
+		}
+		if (this.containsKey("FreeForOps")) {
+			this.OPfree = this.getPropertyBoolean("FreeForOps");
+		}
+		if (this.containsKey("sendprice")) {
+			this.costSend = this.getPropertyInteger("sendprice");
+		}
+		if (this.containsKey("receiveprice")) {
+			this.costReceive = this.getPropertyInteger("receiveprice");
+		}
+		if (this.containsKey("longmessageprice")) {
+			this.costLongSend = this.getPropertyInteger("longmessageprice");
+		}
+		
+		//Check iConomy Settings\\
+		if (this.iConomyEnabled == null) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, Turn_iConomy_ON is not valid. MineMail will use default");
+			this.iConomyEnabled = false;
+		}
+		if (this.OPfree == null) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, FreeForOps is not valid. MineMail will use default");
+			this.OPfree = true;
+		}
+		if (this.costSend == null || this.costSend < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, sendPrice is not valid. MineMail will use default");
+			this.costSend = 10;
+		}
+		if (this.costReceive == null || this.costReceive < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, receivePrice is not valid. MineMail will use default");
+			this.costReceive = 10;
+		}
+		if (this.costLongSend == null || this.costLongSend < 0) {
+			this.plugin.log.warning(this.plugin.logPrefix + " Error in settings, longMessagePrice is not valid. MineMail will use default");
+			this.costLongSend = 10;
+		}
+		
+	}
+	
+	public Boolean containsKey(String key) {
+		return this.FileContents.containsKey(key);
 	}
 	
 	private void delete() {
@@ -97,7 +182,7 @@ public class MainSettingsFile {
 			while((word = br.readLine()) != null) {
 				if ((word.isEmpty()) || (word.startsWith("#")) || (!word.contains(":"))) continue;
 				String[] args = word.split(":");
-				result.put(args[0], args[1]);
+				result.put(args[0].toLowerCase(), args[1]);
 			}
 			
 		} catch (IOException ex) {
@@ -113,7 +198,7 @@ public class MainSettingsFile {
 			load(false);
 		}
 		try {
-			if (FileContents.containsKey(property)) {
+			if (FileContents.containsKey(property.toLowerCase())) {
 				return FileContents.get(property);
 			}
 			
@@ -149,7 +234,6 @@ public class MainSettingsFile {
 		}
 		try {
 			String result = FileContents.get(property);
-			//StruckDown.log.info(property + "=" + result);
 			return Integer.valueOf(result);
 				
 		} catch (Exception e) {
