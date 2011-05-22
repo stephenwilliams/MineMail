146c4a946aaaa5e5a82d102eafbc274e9d15ff18
alta189 <stephendwilliams7@gmail.com>
2011-05-22 16:38:01 -0400

    Fixed More Commandhandler Bugs

--- src/com/alta189/minemail/config/MainSettingsFile.java
+++ src/com/alta189/minemail/config/MainSettingsFile.java
@@ -1,14 +1,11 @@
 package com.alta189.minemail.config;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 
 import com.alta189.minemail.MineMail;
